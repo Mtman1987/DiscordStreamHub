6b6e0e15c413a975e897375026f731aba0806993
Mtman1987 <mtman1987@gmail.com>
2026-08-14 07:43:18 -0500

    Remove floating Personal opacity control

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,10 +1,5 @@
 'use client';
 
-import { PersonalOverlayOpacityControl } from '@/components/personal-overlay-opacity-control';
-
 export default function Template({ children }: { children: React.ReactNode }) {
-  return <>
-    {children}
-    <PersonalOverlayOpacityControl storageKey="discord-stream-hub:personal-overlay-opacity" />
-  </>;
+  return <>{children}</>;
 }
